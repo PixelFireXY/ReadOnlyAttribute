7b84718130a0efc89fd6c1f7970e76dfbc4586b3
Matthew <60183477+PixelFireXY@users.noreply.github.com>
2021-08-31 17:21:44 +0200

    First commit

---
+++ Utils/ReadOnlyAttribute.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Display a field as read-only in the inspector.
+/// </summary>
+/// <seealso cref="ReadOnlyDrawer"/>
+public class ReadOnlyAttribute : PropertyAttribute { }
